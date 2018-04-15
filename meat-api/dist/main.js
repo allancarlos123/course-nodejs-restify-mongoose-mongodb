8af27d69a30d8cd8d5a4d07ba1283981310f7805
Allan <allancarlosdeveloper@gmail.com>
2018-04-15 01:40:13 -0300

    setup

--- meat-api/dist/main.js
+++ meat-api/dist/main.js
@@ -1,1 +1,14 @@
-console.log('main script...');
+"use strict";
+exports.__esModule = true;
+var restify = require("restify");
+var server = restify.createServer({
+    name: "meat-api",
+    version: "1.0.0"
+});
+server.get("/hello", function (req, resp, next) {
+    resp.json({ message: "agora vai..." });
+    return next();
+});
+server.listen(3000, function () {
+    console.log("API is running on http://localhost:3000");
+});
