8870f8c02942302ba88b8ca19834d0842615d24c
Allan <allancarlosdeveloper@gmail.com>
2018-04-15 02:13:56 -0300

    error message

--- meat-api/dist/main.js
+++ meat-api/dist/main.js
@@ -5,10 +5,25 @@ var server = restify.createServer({
     name: "meat-api",
     version: "1.0.0"
 });
-server.get("/hello", function (req, resp, next) {
-    resp.json({ message: "agora vai..." });
-    return next();
-});
+server.use(restify.plugins.queryParser());
+server.get("/info", [
+    function (req, res, next) {
+        var error = new Error();
+        error.statusCode = 400;
+        error.message = "Please, update your browser";
+        return next(error);
+    },
+    function (req, res, next) {
+        res.json({
+            browser: req.userAgent(),
+            method: req.method,
+            url: req.href(),
+            path: req.path(),
+            query: req.query
+        });
+        return next();
+    }
+]);
 server.listen(3000, function () {
     console.log("API is running on http://localhost:3000");
 });
--- meat-api/dist/main.ts
+++ meat-api/dist/main.ts
@@ -5,10 +5,27 @@ const server = restify.createServer({
   version: "1.0.0"
 });
 
-server.get("/hello", (req, resp, next) => {
-  resp.json({ message: "agora vai..." });
-  return next();
-});
+server.use(restify.plugins.queryParser());
+
+server.get("/info", [
+  (req, res, next) => {
+    let error: any = new Error();
+    error.statusCode = 400;
+    error.message = "Please, update your browser";
+    return next(error);
+  },
+  (req, res, next) => {
+    res.json({
+      browser: req.userAgent(),
+      method: req.method,
+      url: req.href(),
+      path: req.path(),
+      query: req.query
+    });
+
+    return next();
+  }
+]);
 
 server.listen(3000, () => {
   console.log("API is running on http://localhost:3000");
